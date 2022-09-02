86891c41c7a8afce008386bd72e06723c8133254
unknown <ignacioduarte099@gmail.com>
2022-09-02 07:39:20 -0300

    Ultimo commit

---
+++ src/com/tema1/main.java
@@ -0,0 +1,43 @@
+package com.tema1;
+
+public class main {
+    public static void main(String[] args) {
+
+
+        // int
+        byte numeroByte = 1;
+        short numeroShort = 20;
+        int numeroInt = 40;
+        long numeroLong = 4589;
+
+        // decimales
+        double numeroDouble = 0.7d;
+        float numeroFlotante = 2.3f;
+
+        // char y texto
+        char caracter1 = 'a';
+        String cadenaDeTexto = "Hola Mundo";
+
+
+        // boolean
+        boolean verdadero = true;
+        boolean falso = false;
+
+
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+        System.out.println(numeroInt);
+        System.out.println(numeroLong);
+        System.out.println(numeroDouble);
+        System.out.println(numeroFlotante);
+        System.out.println(caracter1);
+        System.out.println(cadenaDeTexto);
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+
+
+
+
+    }
+}
