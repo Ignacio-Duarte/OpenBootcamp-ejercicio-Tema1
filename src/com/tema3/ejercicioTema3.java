39eb02d555a2c6c98452b1686b3e2bc3b5f6d910
unknown <ignacioduarte099@gmail.com>
2022-09-03 06:57:49 -0300

    Ejercicio Tema 3

--- src/com/tema3/ejercicioTema3.java
+++ src/com/tema3/ejercicioTema3.java
@@ -1,14 +1,32 @@
 package com.tema3;
 
-import java.sql.SQLOutput;
 
 public class ejercicioTema3 {
-    public static void main(String[] args) {
-        System.out.println("Tema 3");
+
+    /*
+    Array almacen de palabras para generar Texto.
+     */
+    static String[] nombre = new String[]{"Hola", "Mundo", "de", "la", "Programacion"};
+
+    /*
+    La funcion recibe un parametro de tipo Array para ejecutarse.
+    Dentro de la funcion inicializa un bucle for para recorrer el Array
+    Luego Inicializa una variable vacia de tipo String "TextoCompleto" que sirve para almacenar el texto final.
+    Finalmente lo imprime.
+     */
+    public static void creandoTexto(String[] palabras){
+        String textoCompleto = "";
+        for( int i = 0; i < palabras.length; i++){
+            textoCompleto = textoCompleto + " " + palabras[i];
+        }
+        System.out.println(textoCompleto);
     }
 
+    public static void main(String[] args) {
 
+        //Invocando la Funcion.
 
-
+        creandoTexto(nombre);
+    }
 
 }
